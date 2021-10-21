441758aba54b4e0e62147cc2168bbe32e97f3062
Wolfson User <wolfsonuser@ip-172-26-7-135.us-west-2.compute.internal>
2021-10-21 21:43:18 +0000

    Last commit before running matplotlib

--- read-gb-fasta-nucle.py
+++ read-gb-fasta-nucle.py
@@ -1,96 +1,68 @@
-import hashlib
-
-myFile = 'test.txt'
-f = open(myFile)
-lines = f.readlines()
-#print(len(lines))
-count = 0 # total number of lines
-countLines = 0 # number of lines in fasta sections
-build = ""
-buildFasta = ""
-symbolArr = []
-
-for i, l in enumerate(lines):
-    if l[0] == '>':
-        symbolArr.append(i)
-
-for i, l in enumerate(lines):
-#    print(countLines)
-    if i == 0:
-        print(i )
-        print(l)
-        print(hashlib.md5(l.encode()).hexdigest() + '\n')
-        build += l 
-        build += hashlib.md5(l.encode()).hexdigest() + '\n'
-        count += 1
-    elif i <  symbolArr[1] :
-        buildFasta += l
-        
-    elif i == symbolArr[1]:
-        print(buildFasta)
-        print( hashlib.md5(buildFasta.encode()).hexdigest() + '\n')
-        print(len(buildFasta))
-        build += hashlib.md5(buildFasta.encode()).hexdigest() + '\n'
-        build = str(len(buildFasta)) + '\n'
-        buildFasta = ""
-
-    elif l[0] == '>':
-        print("##############")
-        build += l 
-        build += (hashlib.md5(l.encode()).hexdigest() ) + '\n'
-        build += (hashlib.md5(buildFasta.encode()).hexdigest() ) + '\n'
-        build += str(len(buildFasta)) + '\n'
-        print(l)
-        print(hashlib.md5(l.encode()).hexdigest() )
-        print(len(build))
-        print(buildFasta)
-        print(hashlib.md5(buildFasta.encode()).hexdigest() )
-        print(len(buildFasta))
-        buildFasta = ""
-        count += 1
-        countLines = 0
+import hashlib 
+import sys
+import os
+def main():
+#       myPath = './from-ftp-ensemble/'
+   myPath = '../'
+   myFile =  'sequence-homo-38-chr22-coding-nucleotides.txt'
+   myFile = 'Homo_sapiens.GRCh38.cdna.all.fa'
+   filepath = myPath + myFile
+    #sys.argv[1]
+   if not os.path.isfile(filepath):
+       print("File path {} does not exist. Exiting...".format(filepath))
+       sys.exit()
+   numberOfLines = countLines(filepath)
+   with open(filepath) as fp:
+       build = ""
+       buildFasta = ""
+       for i, line in enumerate(fp):
+            if isFirst(i, line):
+                build += '"' + line.strip() + '",'
+                build += getHash(i,line) + ', '
+            elif not isAngle(i,line):
+               buildFasta += line
+               #buildFasta += line.strip()
+               if i == numberOfLines-1:
+                   build += getHash(i, buildFasta)
+            elif isAngle(i, line):
+                build += getHash(i, buildFasta) + '\n'
+                buildFasta = ""
+                build += '"' + line.strip() + '",'
+                build += getHash(i,line) + ', '
+            else:
+                print('######')
+       print(build)
+       fo = open(filepath + ".out", 'w')
+       fo.write(build)
+       fo.close()
+#       saveFile(build)
+    #print(line.strip()) if isFirst(i, line)  else print(i)  TERNARY IF no questionMark\n",
+def saveFile(build):
+    fo = open('test-fasta-9-records.txt.out', 'w')
+    fo.write("this is a test")
+    fo.close()
+def countLines(filepath):
+    fo = open(filepath, 'r').readlines()
+    return len(fo)
+def isLast(i, line):
+    return True
+def getHash(i, labelOrFasta):
+    print(i)
+    return hashlib.md5(labelOrFasta.encode()).hexdigest()
+def isAngle(i, line):
+    if line[0] == ">":
+        return True
     else:
-        buildFasta += l
-        countLines += 1
-        count += 1
+        return False
+def isFirst( i, line):
+    if i == 0:
+       return True
+if __name__ == '__main__':
+    main()
 
-onesequence = '''ATGGAGACAGTGTTTGAAGAGATGGATGAAGAAAGCACAGGAGGAGTTTCATCTTCGAAAGAAGAAATAG
-TCCTTGGCCAGAGACTCCATCTAAGCTTTCCTTTTAGCATTATCTTCTCAACTGTTCTCTACTGTGGTGA
-GGTTGCCTTTGGTTTATACATGTTTGAAATTTATCGAAAAGCTAATGACACATTCTGGATGTCATTTACC
-ATCAGCTTTATTATTGTGGGGGCAATTTTGGATCAAATTATCCTGATGTTTTTCAACAAAGACTTGAGGA
-GAAATAAGGCTGCATTACTTTTTTGGCACATTCTTCTTTTAGGACCTATTGTGAGGTGTTTGCACACCAT
-TAGAAATTACCACAAATGGTTGAAAAATCTTAAACAGGAGAAGGAAGAGACTCAAGTTAGCATCACAAAG
-AGAAACACGATGCTGGAAAGGGAGATTGCATTCTCAATCCGGGATAATTTCATGCAGCAGAAGGCTTTCA
-AGTACATGTCAGTGATTCAGGCTTTTCTCGGTTCTGTTCCACAATTAATTTTGCAGATGTATATCAGTCT
-CACTATACGAGAATGGCCTTTGAATAGAGCATTGCTGATGACATTTTCCCTGTTATCAGTTACTTATGGG
-GCCATTCGCTGCAATATACTGGCCATCCAGATCAGCAATGATGATACTACCATTAAGCTACCGCCGATAG
-AATTCTTCTGTGTCGTGATGTGGCGTTTTTTGGAGGTTATCTCACGTGTAGTGACTCTGGCATTTTTCAT
-TGCATCTCTGAAACTGAAGAGCCTACCCGTTTTGTTAATCATATATTTTGTATCATTGTTGGCACCGTGG
-CTGGAGTTTTGGAAAAGTGGAGCTCATCTTCCTGGCAACAAAGAAAATAATTCCAATATGGTGGGTACAG
-TACTGATGCTTTTCTTGATCACACTGCTATATGCTGCCATCAACTTCTCCTGCTGGTCAGCAGTGAAACT
-GCAGTTGTCAGATGACAAAATAATTGACGGGAGACAGAGGTGGGGCCATAGAATCCTACACTACAGCTTT
-CAGTTTTTAGAAAATGTGATAATGATATTGGTATTTAGGTTCTTTGGAGGGAAAACTTTGCTGAATTGTT
-GTGACTCATTAATTGCCGTGCAGCTCATCATAAGCTACCTATTGGCCACTGGCTTTATGCTCCTCTTCTA
-TCAGTATTTGTACCCATGGCAGTCAGGCAAAGTGTTGCCAGGACGTACTGAAAATCAGCCAGAAGCACCG
-TACTATTATGTAAACATCGAGAAAACTGAAAAGAATAAAAATAAGCAGCTGAGGAATTACTGTCACTCCT
-GCAATAGGGTTGGATATTTTTCAATCAGAAAAAGTATGACATGTTCATAA
-'''
-twosequence = '''ATGGCAGAAGACAAAACCAAACCGAGTGAGTTGGACCAAGGGAAGTATGATGCTGATGACAACGTGAAGA
-TCATCTGCCTGGGAGACAGCGCAGTGGGCAAATCCAAACTCATGGAGAGATTTCTCATGGATGGCTTTCA
-GCCACAGCAGCTGTCCACGTACGCCCTGACCCTGTACAAGCACACAGCCACGGTAGATGGAAGGACCATC
-CTTGTGGACTTTTGGGACACGGCAGGCCAGGAGCGGTTCCAGAGCATGCATGCCTCCTACTACCACAAGG
-CCCACGCCTGCATCATGGCCCTGCTTCAATGCCACCACCTCCGTGCAGCCTCCTCTGATGTTTGTCATTA
-G
 '''
 print(build)
 print(symbolArr)
-
-print(hashlib.md5(onesequence.encode()).hexdigest())
-print(hashlib.md5(twosequence.encode()).hexdigest())
 '''
-for i, l in enumerate(lines):
-    print("Line {}: {}".format(i, l.strip()))
 
-'''
-f.close()
 
